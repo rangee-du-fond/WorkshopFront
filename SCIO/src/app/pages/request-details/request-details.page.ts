c864179af9772628fcf70aa439c1fcd36e48d71b
ymazza <yoann.mazza1@gmail.com>
2020-04-18 00:05:35 +0200

    feat: request details

--- SCIO/src/app/pages/request-details/request-details.page.ts
+++ SCIO/src/app/pages/request-details/request-details.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-request-details',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-details.page.scss'],
 })
 export class RequestDetailsPage implements OnInit {
+  requestid: any;
+  request: any;
+  ionicForm: FormGroup;
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder,private route: ActivatedRoute, private router: Router, public http: HttpClient  ) { 
+    this.requestid = this.router.url.split('/')[2];
+    
+    this.ionicForm = this.formBuilder.group({
+      description: ['', [Validators.required]],
+      courses: ['',[Validators.required]]    
+    })
+  }
 
   ngOnInit() {
+    this.http.get('https://scio-api.herokuapp.com/requests/' + this.requestid )
+    .subscribe(data =>{
+      console.log('data', data);      
+      this.request = data;
+      this.request['created_at'] = this.toDateString(this.request['created_at']);
+    })
   }
+
+  private toDateString(date: Date): string {
+    console.log('date', date);
+    date = new Date(date);
+    return ( ("0" + (date.getDate())).slice(-2)  + '-' 
+       + ("0" + (date.getMonth() + 1)).slice(-2) + '-' 
+       + date.getFullYear().toString() )
+}
+
+
 
 }
